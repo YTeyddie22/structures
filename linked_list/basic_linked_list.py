aec1cc104c46b7068c471de88a9caea5bb4020f6
Teddy Yongo <teyddie@gmail.com>
2025-01-02 08:14:15 +0300

    find lowest value in linked list

--- linked_list/basic_linked_list.py
+++ linked_list/basic_linked_list.py
@@ -3,13 +3,25 @@ class Node:
         self.data = data
         self.next = None
 
-
+# Transversal
 def transverseAndPrint(head):
     currentNode = head
     while currentNode:
         print(currentNode.data,end="-> ")
         currentNode = currentNode.next
     print("null")
+    
+# Finding the lowest value
+
+def findLowestVal(head):
+    minValue = head.data
+    currentNode = head.next
+    while currentNode:
+        if currentNode.data < minValue:
+            minValue = currentNode.data
+        currentNode = currentNode.next
+    return minValue
+    
         
 node1 = Node(3)
 node2 = Node(5)
@@ -21,4 +33,6 @@ node2.next = node3
 node3.next = node4
 
 transverseAndPrint(node1)
+print(findLowestVal(node1))
+
 
